23eef429aed9d962f28a9dacc09f888c335e777f
Mathis Beauville <mathis.beauville@gmail.com>
2021-10-17 10:58:52 +0200

    Initial state

--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
+// Depedencies
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
-
 const app = express();
+const helmet = require('helmet');
+
+// Routes
+const { exampleRouter } = require('./routes/example.routes');
 
 app.use(
     cors({
-        origin: 'http://localhost:3000'
+        origin: 'localhost:3000'
     })
 );
-
-app.get('/', (req, res) => {
-    res.json({'test':'test'});
-});
+app.use(helmet());
+// Local example
+app.use('/api/example', exampleRouter);
 
 // Server Start
 app.listen(process.env.PORT || 3001, () => {
