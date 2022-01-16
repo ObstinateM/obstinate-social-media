6a2b20fb217f945684911b711bf654d917a2b08b
Mathis Beauville <mathis.beauville@gmail.com>
2022-01-16 01:15:03 +0100

    Fx node deploy

--- server/deploy-database.js
+++ server/deploy-database.js
@@ -5,7 +5,7 @@ const option = {
     host: process.env.SQL_HOST,
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
-    database: 'test',
+    database: process.env.SQL_DATABASE,
     port: process.env.SQL_PORT
 };
 
